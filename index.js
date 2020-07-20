519256af960a5256d01448527784087fe7ac3699
pragmatic-founder-4933 <agustinandrew@gmail.com>
2020-07-20 23:02:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ var specialGuest = "Neil deGrasse Tyson"
 
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
-var topic = "Let's talk about space.";
+var topic = "space";
 
-var conversation = `${topic}`;
-
-solution
+var conversation = `let\'s talk about ${topic}`;
