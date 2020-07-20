e2c88158e92089227389dac20ed0a302dc7d4f75
pragmatic-founder-4933 <agustinandrew@gmail.com>
2020-07-20 22:33:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var greeting = "Hello, Everybody!"
 var specialGuest = "Neil deGrasse Tyson"
 
 var greetSpecialGuest = "Hello" + specialGuest + "!"
-
-var topic = "Let's talk about space"
+var space = "Let's talk about space"
+var topic = "space"
 
 var conversation = `${topic}`;
